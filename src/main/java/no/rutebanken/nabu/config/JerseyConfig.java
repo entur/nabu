d66b5e70f14b34f6708467dcd390a806ded1e29c
Tommy Gagnes <tg@scienta.no>
2016-02-04 15:04:25 +0100

    POC-185 Removing keycloak

--- src/main/java/no/rutebanken/nabu/config/JerseyConfig.java
+++ src/main/java/no/rutebanken/nabu/config/JerseyConfig.java
@@ -6,7 +6,10 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
+import javax.ws.rs.ApplicationPath;
+
 @Configuration
+@ApplicationPath("/jersey")
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
