c6b1aa35fbaea95c2d61c4687ac3fd7b5b5b89a3
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-03-18 20:46:22 +0100

    NRP-77 Attempt to populate DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into chouette_info(prefix, data_space, organisation, cuser) values ('rutebanken', 'rutebanken', 'Rutebanken', 'admin@rutebanken.org');
-insert into chouette_info(prefix, data_space, organisation, cuser) values ('tds1', 'testDS1', 'Rutebanken', 'admin@rutebanken.org');
+insert into public.chouette_info(prefix, data_space, organisation, cuser) values ('rutebanken', 'rutebanken', 'Rutebanken', 'admin@rutebanken.org');
+insert into public.chouette_info(prefix, data_space, organisation, cuser) values ('tds1', 'testDS1', 'Rutebanken', 'admin@rutebanken.org');
 
-insert into provider(id, name, sftp_account, chouette_info_id) values (42, 'Flybussekspressen', 'kartverk', 1);
-insert into provider(id, name, sftp_account, chouette_info_id) values (1, 'Rutebanken', 'nvdb', 2);
+insert into public.provider(id, name, sftp_account, chouette_info_id) values (42, 'Flybussekspressen', 'kartverk', 1);
+insert into public.provider(id, name, sftp_account, chouette_info_id) values (1, 'Rutebanken', 'nvdb', 2);
