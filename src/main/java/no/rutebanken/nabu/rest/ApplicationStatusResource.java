deaded7a808d8f1e6b09353acf43807a61f43689
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-03-21 14:30:11 +0100

    NRP-161 Adjusting log level for probes

--- src/main/java/no/rutebanken/nabu/rest/ApplicationStatusResource.java
+++ src/main/java/no/rutebanken/nabu/rest/ApplicationStatusResource.java
@@ -29,7 +29,7 @@ public class ApplicationStatusResource {
     @GET
     @Path("/ready")
     public Response isReady() {
-        logger.info("Checking DB connections...");
+        logger.debug("Checking DB connections...");
         if ( ((DbStatus) providerRepository).isDbUp() && ((DbStatus) statusRepository).isDbUp()){
            return Response.ok().build();
         } else {
