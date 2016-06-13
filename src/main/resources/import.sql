7991fd0d2c49db8873e88c0e643c291660af746c
Arne Seime <arne.seime@greenbird.com>
2016-06-13 11:51:05 +0200

    Separated telemark and vestfold

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,6 +15,7 @@ insert into public.chouette_info(id, prefix, referential, organisation, cuser, r
 insert into public.chouette_info(id, prefix, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection) values (15, 'TRO', 'tro', 'Rutebanken', 'admin@rutebanken.org', null, 'EPSG:32633');
 insert into public.chouette_info(id, prefix, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection) values (16, 'FIN', 'fin', 'Rutebanken', 'admin@rutebanken.org', null, 'EPSG:32633');
 insert into public.chouette_info(id, prefix, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection) values (17, 'TOG', 'tog', 'Rutebanken', 'admin@rutebanken.org', null, 'EPSG:4326'); --THIS IS ALSO CORRECT
+insert into public.chouette_info(id, prefix, referential, organisation, cuser, regtopp_version, regtopp_coordinate_projection) values (18, 'TEL', 'telemark', 'Rutebanken', 'admin@rutebanken.org', null, 'EPSG:32632');
 
 
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (1, 'Østfold / Østfold Kollektivtrafikk', 'ostfold', 1);
@@ -22,7 +23,7 @@ insert into public.provider(id, name, sftp_account, chouette_info_id) values (2,
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (3, 'Hedmark / Hedmark-Trafikk', 'hedmark', 3);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (4, 'Oppland / Opplandstrafikk', 'oppland', 4);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (5, 'Buskerud / Brakar', 'buskerud', 5);
-insert into public.provider(id, name, sftp_account, chouette_info_id) values (6, 'Vestfold og Telemark / VkT', 'vestfold-telemark', 6);
+insert into public.provider(id, name, sftp_account, chouette_info_id) values (6, 'Vestfold / VkT', 'vestfold', 6);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (7, 'Agder / AkT', 'agder', 7);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (8, 'Rogaland / Kolumbus', 'rogaland', 8);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (9, 'Hordaland / Skyss', 'hordaland', 9);
@@ -34,3 +35,4 @@ insert into public.provider(id, name, sftp_account, chouette_info_id) values (14
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (15, 'Troms / Troms Fylkestrafikk', 'troms', 15);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (16, 'Finnmark / Snelandia', 'finnmark', 16);
 insert into public.provider(id, name, sftp_account, chouette_info_id) values (17, 'Tog / NSB', 'tog', 17);
+insert into public.provider(id, name, sftp_account, chouette_info_id) values (18, 'Telemark', 'telemark', 18);
