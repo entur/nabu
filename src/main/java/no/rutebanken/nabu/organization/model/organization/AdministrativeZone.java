a01e777cad5d239fb0654d2e9c6c75bfe4e0ca8b
erlendnils1 <erlendnils1@gmail.com>
2017-03-22 08:43:27 +0100

    NRP-1294 Lazy load polygons

--- src/main/java/no/rutebanken/nabu/organization/model/organization/AdministrativeZone.java
+++ src/main/java/no/rutebanken/nabu/organization/model/organization/AdministrativeZone.java
@@ -3,10 +3,7 @@ package no.rutebanken.nabu.organization.model.organization;
 import com.vividsolutions.jts.geom.Polygon;
 import no.rutebanken.nabu.organization.model.CodeSpaceEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -18,6 +15,7 @@ public class AdministrativeZone extends CodeSpaceEntity {
 	@NotNull
 	private String name;
 
+	@Basic(fetch = FetchType.LAZY)
 	@Column(columnDefinition = "geometry")
 	@NotNull
 	private Polygon polygon;
