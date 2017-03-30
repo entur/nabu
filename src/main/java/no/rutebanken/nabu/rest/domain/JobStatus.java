6d6bf203f24be8ab0fb86f21e6e3458c76833238
erlendnils1 <erlendnils1@gmail.com>
2017-03-30 16:18:57 +0200

    Added new status extion for netex export

--- src/main/java/no/rutebanken/nabu/rest/domain/JobStatus.java
+++ src/main/java/no/rutebanken/nabu/rest/domain/JobStatus.java
@@ -10,89 +10,89 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 @JsonRootName("jobs")
 public class JobStatus {
 
-	public enum Action {
-		FILE_TRANSFER, FILE_CLASSIFICATION, IMPORT, EXPORT,  VALIDATION_LEVEL_1, VALIDATION_LEVEL_2, CLEAN, DATASPACE_TRANSFER, BUILD_GRAPH
-	}
+    public enum Action {
+        FILE_TRANSFER, FILE_CLASSIFICATION, IMPORT, EXPORT, VALIDATION_LEVEL_1, VALIDATION_LEVEL_2, CLEAN, DATASPACE_TRANSFER, BUILD_GRAPH, EXPORT_NETEX
+    }
 
-	public enum State {
-		PENDING, STARTED, TIMEOUT, FAILED, OK, DUPLICATE
-	}
+    public enum State {
+        PENDING, STARTED, TIMEOUT, FAILED, OK, DUPLICATE
+    }
 
-	@JsonProperty("events")
-	private List<JobStatusEvent> events = new ArrayList<JobStatusEvent>();
+    @JsonProperty("events")
+    private List<JobStatusEvent> events = new ArrayList<JobStatusEvent>();
 
-	@JsonProperty("correlationId")
-	private String correlationId;
+    @JsonProperty("correlationId")
+    private String correlationId;
 
-	@JsonProperty("firstEvent")
-	private Date firstEvent;
+    @JsonProperty("firstEvent")
+    private Date firstEvent;
 
-	@JsonProperty("lastEvent")
-	private Date lastEvent;
+    @JsonProperty("lastEvent")
+    private Date lastEvent;
 
-	@JsonProperty("durationMillis")
-	private Long durationMillis;
+    @JsonProperty("durationMillis")
+    private Long durationMillis;
 
-	@JsonProperty("endState")
-	private State endStatus;
+    @JsonProperty("endState")
+    private State endStatus;
 
-	@JsonProperty("fileName")
-	public String fileName;
+    @JsonProperty("fileName")
+    public String fileName;
 
-	public List<JobStatusEvent> getEvents() {
-		return events;
-	}
+    public List<JobStatusEvent> getEvents() {
+        return events;
+    }
 
-	public void addEvent(JobStatusEvent event) {
-		events.add(event);
-	}
+    public void addEvent(JobStatusEvent event) {
+        events.add(event);
+    }
 
-	public String getCorrelationId() {
-		return correlationId;
-	}
+    public String getCorrelationId() {
+        return correlationId;
+    }
 
-	public void setCorrelationId(String correlationId) {
-		this.correlationId = correlationId;
-	}
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
 
-	public Date getFirstEvent() {
-		return firstEvent;
-	}
+    public Date getFirstEvent() {
+        return firstEvent;
+    }
 
-	public void setFirstEvent(Date firstEvent) {
-		this.firstEvent = firstEvent;
-	}
+    public void setFirstEvent(Date firstEvent) {
+        this.firstEvent = firstEvent;
+    }
 
-	public Date getLastEvent() {
-		return lastEvent;
-	}
+    public Date getLastEvent() {
+        return lastEvent;
+    }
 
-	public void setLastEvent(Date lastEvent) {
-		this.lastEvent = lastEvent;
-	}
+    public void setLastEvent(Date lastEvent) {
+        this.lastEvent = lastEvent;
+    }
 
-	public State getEndStatus() {
-		return endStatus;
-	}
+    public State getEndStatus() {
+        return endStatus;
+    }
 
-	public void setEndStatus(State endStatus) {
-		this.endStatus = endStatus;
-	}
+    public void setEndStatus(State endStatus) {
+        this.endStatus = endStatus;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
-	public Long getDurationMillis() {
-		return durationMillis;
-	}
+    public Long getDurationMillis() {
+        return durationMillis;
+    }
 
-	public void setDurationMillis(Long durationMillis) {
-		this.durationMillis = durationMillis;
-	}
+    public void setDurationMillis(Long durationMillis) {
+        this.durationMillis = durationMillis;
+    }
 
 }
