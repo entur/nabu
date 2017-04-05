6221187f29b101c45c2edf980d49c42b2f84facc
erlendnils1 <erlendnils1@gmail.com>
2017-04-05 09:00:57 +0200

    Fixed cascading delete for responsibility sets

--- src/main/java/no/rutebanken/nabu/organisation/model/responsibility/ResponsibilityRoleAssignment.java
+++ src/main/java/no/rutebanken/nabu/organisation/model/responsibility/ResponsibilityRoleAssignment.java
@@ -12,106 +12,113 @@ import java.util.Set;
 
 @Entity
 @Table(uniqueConstraints = {
-		                           @UniqueConstraint(name = "rsp_role_assignment_unique_id", columnNames = {"code_space_pk","privateCode", "entityVersion"})
+                                   @UniqueConstraint(name = "rsp_role_assignment_unique_id", columnNames = {"code_space_pk", "privateCode", "entityVersion"})
 })
 public class ResponsibilityRoleAssignment extends CodeSpaceEntity {
 
-	@NotNull
-	@ManyToOne
-	private Role typeOfResponsibilityRole;
+    @NotNull
+    @ManyToOne
+    private Role typeOfResponsibilityRole;
 
-	@NotNull
-	@ManyToOne
-	private Organisation responsibleOrganisation;
+    @NotNull
+    @ManyToOne
+    private Organisation responsibleOrganisation;
 
-	@ManyToOne
-	private AdministrativeZone responsibleArea;
-
-
-	@ManyToMany
-	@JoinTable(name="RESPONSIBILITY_ROLE_ASSIGNMENT_ENTITY_CLASSIFICATIONS")
-	private Set<EntityClassification> responsibleEntityClassifications;
+    @ManyToOne
+    private AdministrativeZone responsibleArea;
 
 
-	public Role getTypeOfResponsibilityRole() {
-		return typeOfResponsibilityRole;
-	}
-
-	public void setTypeOfResponsibilityRole(Role typeOfResponsibilityRole) {
-		this.typeOfResponsibilityRole = typeOfResponsibilityRole;
-	}
-
-	public Organisation getResponsibleOrganisation() {
-		return responsibleOrganisation;
-	}
-
-	public void setResponsibleOrganisation(Organisation responsibleOrganisation) {
-		this.responsibleOrganisation = responsibleOrganisation;
-	}
-
-	public AdministrativeZone getResponsibleArea() {
-		return responsibleArea;
-	}
-
-	public void setResponsibleArea(AdministrativeZone responsibleArea) {
-		this.responsibleArea = responsibleArea;
-	}
-
-	public Set<EntityClassification> getResponsibleEntityClassifications() {
-		if (responsibleEntityClassifications == null) {
-			this.responsibleEntityClassifications = new HashSet<>();
-		}
-		return responsibleEntityClassifications;
-	}
-
-	public void setResponsibleEntityClassifications(Set<EntityClassification> responsibleEntityClassifications) {
-		getResponsibleEntityClassifications().clear();
-		getResponsibleEntityClassifications().addAll(responsibleEntityClassifications);
-	}
-
-	public static Builder builder() {
-		return new Builder();
-	}
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+    @JoinTable(name = "RESPONSIBILITY_ROLE_ASSIGNMENT_ENTITY_CLASSIFICATIONS")
+    private Set<EntityClassification> responsibleEntityClassifications;
 
 
-	public static class Builder {
-		private ResponsibilityRoleAssignment target = new ResponsibilityRoleAssignment();
+    public Role getTypeOfResponsibilityRole() {
+        return typeOfResponsibilityRole;
+    }
 
-		public Builder withPrivateCode(String privateCode) {
-			target.setPrivateCode(privateCode);
-			return this;
-		}
+    public void setTypeOfResponsibilityRole(Role typeOfResponsibilityRole) {
+        this.typeOfResponsibilityRole = typeOfResponsibilityRole;
+    }
 
-		public Builder withCodeSpace(CodeSpace codeSpace) {
-			target.setCodeSpace(codeSpace);
-			return this;
-		}
+    public Organisation getResponsibleOrganisation() {
+        return responsibleOrganisation;
+    }
 
-		public Builder withResponsibleOrganisation(Organisation responsibleOrganisation) {
-			target.setResponsibleOrganisation(responsibleOrganisation);
-			return this;
-		}
+    public void setResponsibleOrganisation(Organisation responsibleOrganisation) {
+        this.responsibleOrganisation = responsibleOrganisation;
+    }
 
-		public Builder withResponsibleArea(AdministrativeZone responsibleArea) {
-			target.setResponsibleArea(responsibleArea);
-			return this;
-		}
+    public AdministrativeZone getResponsibleArea() {
+        return responsibleArea;
+    }
 
-		public Builder withTypeOfResponsibilityRole(Role typeOfResponsibilityRole) {
-			target.setTypeOfResponsibilityRole(typeOfResponsibilityRole);
-			return this;
-		}
+    public void setResponsibleArea(AdministrativeZone responsibleArea) {
+        this.responsibleArea = responsibleArea;
+    }
 
-		public Builder withResponsibleEntityClassifications(Set<EntityClassification> responsibleEntityClassifications) {
-			target.setResponsibleEntityClassifications(responsibleEntityClassifications);
-			return this;
-		}
+    public Set<EntityClassification> getResponsibleEntityClassifications() {
+        if (responsibleEntityClassifications == null) {
+            this.responsibleEntityClassifications = new HashSet<>();
+        }
+        return responsibleEntityClassifications;
+    }
+
+    public void setResponsibleEntityClassifications(Set<EntityClassification> responsibleEntityClassifications) {
+        getResponsibleEntityClassifications().clear();
+        getResponsibleEntityClassifications().addAll(responsibleEntityClassifications);
+    }
+
+    @PreRemove
+    private void removeResponsibilitySetConnections() {
+        if (responsibleEntityClassifications != null) {
+            responsibleEntityClassifications.clear();
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 
 
-		public ResponsibilityRoleAssignment build() {
-			return target;
-		}
-	}
+    public static class Builder {
+        private ResponsibilityRoleAssignment target = new ResponsibilityRoleAssignment();
+
+        public Builder withPrivateCode(String privateCode) {
+            target.setPrivateCode(privateCode);
+            return this;
+        }
+
+        public Builder withCodeSpace(CodeSpace codeSpace) {
+            target.setCodeSpace(codeSpace);
+            return this;
+        }
+
+        public Builder withResponsibleOrganisation(Organisation responsibleOrganisation) {
+            target.setResponsibleOrganisation(responsibleOrganisation);
+            return this;
+        }
+
+        public Builder withResponsibleArea(AdministrativeZone responsibleArea) {
+            target.setResponsibleArea(responsibleArea);
+            return this;
+        }
+
+        public Builder withTypeOfResponsibilityRole(Role typeOfResponsibilityRole) {
+            target.setTypeOfResponsibilityRole(typeOfResponsibilityRole);
+            return this;
+        }
+
+        public Builder withResponsibleEntityClassifications(Set<EntityClassification> responsibleEntityClassifications) {
+            target.setResponsibleEntityClassifications(responsibleEntityClassifications);
+            return this;
+        }
+
+
+        public ResponsibilityRoleAssignment build() {
+            return target;
+        }
+    }
 }
 
 
--- src/main/java/no/rutebanken/nabu/organisation/model/responsibility/ResponsibilitySet.java
+++ src/main/java/no/rutebanken/nabu/organisation/model/responsibility/ResponsibilitySet.java
@@ -12,63 +12,63 @@ import java.util.Set;
 
 @Entity
 @Table(uniqueConstraints = {
-		                           @UniqueConstraint(name = "responsibility_set_unique_id", columnNames = {"code_space_pk","privateCode", "entityVersion"})
+                                   @UniqueConstraint(name = "responsibility_set_unique_id", columnNames = {"code_space_pk", "privateCode", "entityVersion"})
 })
 public class ResponsibilitySet extends CodeSpaceEntity {
 
-	@NotNull
-	private String name;
+    @NotNull
+    private String name;
 
-	@NotNull
-	@ManyToMany(cascade = CascadeType.ALL)
-	private Set<ResponsibilityRoleAssignment> roles;
+    @NotNull
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ResponsibilityRoleAssignment> roles;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Set<ResponsibilityRoleAssignment> getRoles() {
-		if (roles == null) {
-			this.roles = new HashSet<>();
-		}
-		return roles;
-	}
+    public Set<ResponsibilityRoleAssignment> getRoles() {
+        if (roles == null) {
+            this.roles = new HashSet<>();
+        }
+        return roles;
+    }
 
-	public void setRoles(Set<ResponsibilityRoleAssignment> roles) {
-		this.roles = roles;
-	}
+    public void setRoles(Set<ResponsibilityRoleAssignment> roles) {
+        this.roles = roles;
+    }
 
-	public ResponsibilitySet() {
-	}
+    public ResponsibilitySet() {
+    }
 
-	public ResponsibilitySet(CodeSpace codeSpace, String privateCode, String name, Set<ResponsibilityRoleAssignment> roles) {
-		setCodeSpace(codeSpace);
-		setPrivateCode(privateCode);
-		this.name = name;
-		this.roles = roles;
-	}
+    public ResponsibilitySet(CodeSpace codeSpace, String privateCode, String name, Set<ResponsibilityRoleAssignment> roles) {
+        setCodeSpace(codeSpace);
+        setPrivateCode(privateCode);
+        this.name = name;
+        this.roles = roles;
+    }
 
-	public ResponsibilityRoleAssignment getResponsibilityRoleAssignment(String id) {
-		if (id != null && !CollectionUtils.isEmpty(roles)) {
-			for (ResponsibilityRoleAssignment existingRole : roles) {
-				if (id.equals(existingRole.getId())) {
-					return existingRole;
-				}
-			}
-		}
-		throw new IllegalArgumentException(getClass().getSimpleName() + " with id: " + id + " not found");
-	}
+    public ResponsibilityRoleAssignment getResponsibilityRoleAssignment(String id) {
+        if (id != null && !CollectionUtils.isEmpty(roles)) {
+            for (ResponsibilityRoleAssignment existingRole : roles) {
+                if (id.equals(existingRole.getId())) {
+                    return existingRole;
+                }
+            }
+        }
+        throw new IllegalArgumentException(getClass().getSimpleName() + " with id: " + id + " not found");
+    }
 
-	@PreRemove
-	private void removeResponsibilitySetConnections() {
-		if (roles != null) {
-			roles.clear();
-		}
-	}
+    @PreRemove
+    private void removeResponsibilitySetConnections() {
+        if (roles != null) {
+            roles.clear();
+        }
+    }
 
 
 }
