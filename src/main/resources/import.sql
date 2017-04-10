e5a64a595b7f03718039d5be0db35022b46d8818
erlendnils1 <erlendnils1@gmail.com>
2017-04-10 09:25:02 +0200

    Replaced test data with actual organisations

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -104,6 +104,8 @@ insert into code_space (pk, entity_version,lock_version,private_code, xmlns,xmln
 insert into code_space (pk, entity_version,lock_version,private_code, xmlns,xmlns_url) VALUES (2,1,1,'rut','RUT','http://www.rutebanken.org/ns/rut');
 insert into code_space (pk, entity_version,lock_version,private_code, xmlns,xmlns_url) VALUES (3,1,1,'rb','RB','http://www.rutebanken.org/ns/rb');
 
+
+
 insert into role (pk, entity_version,lock_version,private_code,name) VALUES (1,1,1,'editStops','Opplasting og redigering av stopp');
 insert into role (pk, entity_version,lock_version,private_code,name) VALUES (2,1,1,'editPlaceOfInterest','Opplasting og redigering av POI');
 
@@ -115,18 +117,29 @@ insert into entity_classification (pk, entity_version,lock_version,entity_type_p
 insert into entity_classification (pk, entity_version,lock_version,entity_type_pk,code_space_pk,private_code,name) VALUES (2,1,1,1,1,'onstreetTram','Tram stop');
 insert into entity_classification (pk, entity_version,lock_version,entity_type_pk,code_space_pk,private_code,name) VALUES (3,1,1,2,1,'*','All placeOfInterest');
 
-insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'testOrg','Test Org');
-
-insert into responsibility_role_assignment (pk,entity_version,lock_version,private_code,code_space_pk,responsible_organisation_pk,type_of_responsibility_role_pk) VALUES (1,1,1,'1',1,1,1);
-insert into responsibility_role_assignment (pk,entity_version,lock_version,private_code,code_space_pk,responsible_organisation_pk,type_of_responsibility_role_pk) VALUES (2,1,1,'2',1,1,2);
-
-insert into RESPONSIBILITY_ROLE_ASSIGNMENT_ENTITY_CLASSIFICATIONS (responsibility_role_assignment_pk, responsible_entity_classifications_pk) values (1,1);
-insert into RESPONSIBILITY_ROLE_ASSIGNMENT_ENTITY_CLASSIFICATIONS (responsibility_role_assignment_pk, responsible_entity_classifications_pk) values (1,3);
-
-
-insert into responsibility_set (pk,entity_version,lock_version,private_code,code_space_pk,name) values (1,1,1,'1',1,'Test rsp set');
-
-insert into responsibility_set_roles (responsibility_Set_pk,roles_pk) values (1,1);
-insert into responsibility_set_roles (responsibility_Set_pk,roles_pk) values (1,2);
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'OST','Østfold / Østfold Kollektivtrafikk');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'RUT','Oslo og Akershus / Ruter');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'HED','Hedmark / Hedmark-Trafikk');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'OPP','Oppland / Opplandstrafikk');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'BRA','Buskerud / Brakar');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'VKT','Vestfold / VkT');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'AKT','Agder / AkT');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'KOL','Rogaland / Kolumbus');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'SKY','Hordaland / Skyss');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'SOF', 'Sogn og Fjordane / Kringom');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'MOR','Møre og Romsdal / Fram');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'ATB','Sør-Trøndelag / AtB');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'NTR','Nord-Trøndelag');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'NOR','Nordland');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'TRO','Troms / Troms Fylkestrafikk');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'FIN','Finnmark / Snelandia');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'NSB','NSB');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'TEL','Telemark');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'NRI','Norsk ReiseInformasjon');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'NWY','NOR-WAY Bussekspress');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'AVI','Avinor');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'FLT','Flytoget');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'Test1','Test Organisasjon 1');
+insert into organisation(pk, dtype,entity_version,lock_version,code_space_pk,private_code,name) values (nextval('hibernate_sequence'),'Authority',1,1,1,'Test2','Test Organisasjon 2');
 
 alter sequence hibernate_sequence restart with 20000;
