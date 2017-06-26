5a07499cfdb61f44a95ce7863fe367d2048bbec4
erlendnils1 <erlendnils1@gmail.com>
2017-06-26 11:24:34 +0200

    NRP-1788 Store admin zone type as string

--- src/main/java/no/rutebanken/nabu/organisation/model/organisation/AdministrativeZone.java
+++ src/main/java/no/rutebanken/nabu/organisation/model/organisation/AdministrativeZone.java
@@ -22,6 +22,7 @@ public class AdministrativeZone extends CodeSpaceEntity {
 	private Polygon polygon;
 
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private AdministrativeZoneType administrativeZoneType;
 
 	public String getName() {
