88c9fed66fa3f9edd82e474cba3d2f45800a18ef
vincent <vincent.paturet@entur.org>
2020-12-07 11:27:08 +0100

    Update quartz config

--- src/main/java/no/rutebanken/nabu/config/QuartzSchedulerConfig.java
+++ src/main/java/no/rutebanken/nabu/config/QuartzSchedulerConfig.java
@@ -19,23 +19,13 @@ import no.rutebanken.nabu.event.email.EmailSenderJob;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.SimpleTrigger;
-import org.quartz.Trigger;
-import org.quartz.spi.JobFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
 
 /**
  * Based on spring boot quartz example.
