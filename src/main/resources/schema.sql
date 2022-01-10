c975fecbb1ccad410503d74699db21631bbcdb2d
vincent <vincent.paturet@entur.org>
2022-01-10 11:39:43 +0100

    Update SQL init script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -148,29 +148,28 @@ CREATE TABLE IF NOT EXISTS  qrtz_locks
     PRIMARY KEY (SCHED_NAME,LOCK_NAME)
 );
 
--- TODO  --CREATE INDEX IF NOT EXISTS NOT SUPPORTED in 9.4, requires postgres 9.5.. Add manually until upgrade (or skip. only one job)
--- create INDEX IF NOT EXISTS idx_qrtz_j_req_recovery on qrtz_job_details(SCHED_NAME,REQUESTS_RECOVERY);
--- create INDEX IF NOT EXISTS idx_qrtz_j_grp on qrtz_job_details(SCHED_NAME,JOB_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_j_req_recovery on qrtz_job_details(SCHED_NAME,REQUESTS_RECOVERY);
+create INDEX IF NOT EXISTS idx_qrtz_j_grp on qrtz_job_details(SCHED_NAME,JOB_GROUP);
 --
--- create INDEX IF NOT EXISTS idx_qrtz_t_j on qrtz_triggers(SCHED_NAME,JOB_NAME,JOB_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_t_jg on qrtz_triggers(SCHED_NAME,JOB_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_t_c on qrtz_triggers(SCHED_NAME,CALENDAR_NAME);
--- create INDEX IF NOT EXISTS idx_qrtz_t_g on qrtz_triggers(SCHED_NAME,TRIGGER_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_t_state on qrtz_triggers(SCHED_NAME,TRIGGER_STATE);
--- create INDEX IF NOT EXISTS idx_qrtz_t_n_state on qrtz_triggers(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP,TRIGGER_STATE);
--- create INDEX IF NOT EXISTS idx_qrtz_t_n_g_state on qrtz_triggers(SCHED_NAME,TRIGGER_GROUP,TRIGGER_STATE);
--- create INDEX IF NOT EXISTS idx_qrtz_t_next_fire_time on qrtz_triggers(SCHED_NAME,NEXT_FIRE_TIME);
--- create INDEX IF NOT EXISTS idx_qrtz_t_nft_st on qrtz_triggers(SCHED_NAME,TRIGGER_STATE,NEXT_FIRE_TIME);
--- create INDEX IF NOT EXISTS idx_qrtz_t_nft_misfire on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME);
--- create INDEX IF NOT EXISTS idx_qrtz_t_nft_st_misfire on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_STATE);
--- create INDEX IF NOT EXISTS idx_qrtz_t_nft_st_misfire_grp on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_GROUP,TRIGGER_STATE);
+create INDEX IF NOT EXISTS idx_qrtz_t_j on qrtz_triggers(SCHED_NAME,JOB_NAME,JOB_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_t_jg on qrtz_triggers(SCHED_NAME,JOB_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_t_c on qrtz_triggers(SCHED_NAME,CALENDAR_NAME);
+create INDEX IF NOT EXISTS idx_qrtz_t_g on qrtz_triggers(SCHED_NAME,TRIGGER_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_t_state on qrtz_triggers(SCHED_NAME,TRIGGER_STATE);
+create INDEX IF NOT EXISTS idx_qrtz_t_n_state on qrtz_triggers(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP,TRIGGER_STATE);
+create INDEX IF NOT EXISTS idx_qrtz_t_n_g_state on qrtz_triggers(SCHED_NAME,TRIGGER_GROUP,TRIGGER_STATE);
+create INDEX IF NOT EXISTS idx_qrtz_t_next_fire_time on qrtz_triggers(SCHED_NAME,NEXT_FIRE_TIME);
+create INDEX IF NOT EXISTS idx_qrtz_t_nft_st on qrtz_triggers(SCHED_NAME,TRIGGER_STATE,NEXT_FIRE_TIME);
+create INDEX IF NOT EXISTS idx_qrtz_t_nft_misfire on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME);
+create INDEX IF NOT EXISTS idx_qrtz_t_nft_st_misfire on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_STATE);
+create INDEX IF NOT EXISTS idx_qrtz_t_nft_st_misfire_grp on qrtz_triggers(SCHED_NAME,MISFIRE_INSTR,NEXT_FIRE_TIME,TRIGGER_GROUP,TRIGGER_STATE);
 --
--- create INDEX IF NOT EXISTS idx_qrtz_ft_trig_inst_name on qrtz_fired_triggers(SCHED_NAME,INSTANCE_NAME);
--- create INDEX IF NOT EXISTS idx_qrtz_ft_inst_job_req_rcvry on qrtz_fired_triggers(SCHED_NAME,INSTANCE_NAME,REQUESTS_RECOVERY);
--- create INDEX IF NOT EXISTS idx_qrtz_ft_j_g on qrtz_fired_triggers(SCHED_NAME,JOB_NAME,JOB_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_ft_jg on qrtz_fired_triggers(SCHED_NAME,JOB_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_ft_t_g on qrtz_fired_triggers(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP);
--- create INDEX IF NOT EXISTS idx_qrtz_ft_tg on qrtz_fired_triggers(SCHED_NAME,TRIGGER_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_ft_trig_inst_name on qrtz_fired_triggers(SCHED_NAME,INSTANCE_NAME);
+create INDEX IF NOT EXISTS idx_qrtz_ft_inst_job_req_rcvry on qrtz_fired_triggers(SCHED_NAME,INSTANCE_NAME,REQUESTS_RECOVERY);
+create INDEX IF NOT EXISTS idx_qrtz_ft_j_g on qrtz_fired_triggers(SCHED_NAME,JOB_NAME,JOB_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_ft_jg on qrtz_fired_triggers(SCHED_NAME,JOB_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_ft_t_g on qrtz_fired_triggers(SCHED_NAME,TRIGGER_NAME,TRIGGER_GROUP);
+create INDEX IF NOT EXISTS idx_qrtz_ft_tg on qrtz_fired_triggers(SCHED_NAME,TRIGGER_GROUP);
 
 
 commit;
