5ebf2a6f401bd539e76c1fc65e932b9c979f2ba2
henrik242 <henrik242@users.noreply.github.com>
2026-05-19 08:52:24 +0200

    Allow x-correlation-id, et-client-name, baggage, sentry-trace in CORS (#627)
    https://github.com/entur/marduk-pipeline

--- src/main/java/no/rutebanken/nabu/filter/CorsResponseFilter.java
+++ src/main/java/no/rutebanken/nabu/filter/CorsResponseFilter.java
@@ -26,7 +26,7 @@ public class CorsResponseFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) {
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-correlation-id, et-client-name, baggage, sentry-trace");
         response.getHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
